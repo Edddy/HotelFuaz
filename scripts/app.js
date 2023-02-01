5cf6b2b73589a2ab006249a0e9b101e90df0c9d6
Eduardo Molteni <eduardo@bizcacha.com>
2023-02-01 16:44:57 -0300

    Finalizo ultiumos toques

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,28 @@
 ; (function ($) {
 	var app = $.sammy(function () {
 		this.get('/', function () {
-			$('#main').load('home.html');
+			$('#content').load('home.html');
 		});
 
 		this.get('#/contactenos', function () {
-			$('#main').load('contactenos.html');
+			$('#content').load('contactenos.html');
 		});
-		this.get('#/constructora', function () {
-			$('#main').load('constructora.html');
+		this.get('#/habitaciones', function () {
+			$('#content').load('habitaciones.html');
 		});
-		this.get('#/estilos', function () {
-			$('#main').load('estilos.html');
+		this.get('#/servicios', function () {
+			$('#content').load('servicios.html');
 		});
-		this.get('#/trabajosrealizados', function () {
-			$('#main').load('trabajosrealizados.html');
+		this.get('#/ubicacion', function () {
+			$('#content').load('ubicacion.html');
 		});
-		this.get('#/otrosservicios', function () {
-			$('#main').load('otrosservicios.html');
-		});
-		this.get('#/quienessomos', function () {
-			$('#main').load('quienessomos.html');
+		this.get('#/galeria', function () {
+			$('#content').load('galeria.html');
 		});
 	});
 
 	$(function () {
 		app.run();
-
-		$('#fotos').innerfade({
-			animationtype: 'fade',
-			speed: 750,
-			timeout: 4000,
-			type: 'random',
-			containerheight: '300px'
-		});
-		
 	});
 
 	
